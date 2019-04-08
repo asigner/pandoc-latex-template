40803cd265f49d0bb11dce3fd1143fbb15a64e90
Andy Signer <andy@signer.info>
2019-04-08 23:04:44 +0200

    Simple wrapper script for docker

--- eisvogel.sh
+++ eisvogel.sh
@@ -1,3 +1,5 @@
 #/usr/bin/env bash
 
-docker run --rm --volume "`pwd`:/data" --user `id -u`:`id -g` eisvogel:latest --template eisvogel $@
+#docker run --rm --volume "`pwd`:/data" --user `id -u`:`id -g` eisvogel:latest --template eisvogel $@
+
+docker run --rm --volume "`pwd`:/data"  pandoc:eisvogel --template eisvogel $@
